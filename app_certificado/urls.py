7cc3445fdcb9791eb5469de6356a0dce4144b86b
Caique Teles <caiquetm@hotmail.com>
2023-08-21 22:52:53 -0300

    adicionado mais elementos na view

--- app_certificado/urls.py
+++ app_certificado/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from app_certificado.views import home
+from app_certificado.views import home, aluno, usuario, certificado, template
 
 urlpatterns = [
     path('', home),
+    path('', aluno),
+    path('', usuario),
+    path('', certificado),
+    path('', template),
 ]
--- app_certificado/views.py
+++ app_certificado/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 def home(request):
     return render(request, 'app_certificado/home.html')
+
+def aluno(request):
+    return render(request, 'app_certificado/aluno.html')
+
+def usuario(request):
+    return render(request, 'app_certificado/usuario.html')
+
+def certificado(request):
+    return render(request, 'app_certificado/certificado.html')
+
+def template(request):
+    return render(request, 'app_certificado/template.html')
